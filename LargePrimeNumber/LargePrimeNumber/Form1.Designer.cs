e06fa038a47c1f707d8dbdff0cde29c0b5af55d7
llimacruz <leonardo.cruz@outlook.com>
2016-04-03 11:45:03 -0300

    Prime numbers program

---
+++ LargePrimeNumber/LargePrimeNumber/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LargePrimeNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtDigits = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(25, 22);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(25, 125);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstNumbers.TabIndex = 1;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(22, 223);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(13, 13);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "0";
+            // 
+            // txtDigits
+            // 
+            this.txtDigits.Location = new System.Drawing.Point(67, 61);
+            this.txtDigits.Name = "txtDigits";
+            this.txtDigits.Size = new System.Drawing.Size(33, 20);
+            this.txtDigits.TabIndex = 3;
+            this.txtDigits.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Digits:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDigits);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtDigits;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LargePrimeNumber/LargePrimeNumber/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LargePrimeNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //lstNumbers.Items.Clear();
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            int digits = 1;
+            int.TryParse(txtDigits.Text, out digits);
+
+            int maxPrime = FindPrimeNumbers(digits);
+
+            watch.Stop();
+
+            lstNumbers.Items.Add(maxPrime);
+
+            lblTime.Text = watch.Elapsed.TotalSeconds.ToString();
+        }
+
+        int FindPrimeNumbers(int digits)
+        {
+            int prime = 1;
+            int maxPrime = 1;
+            while (prime.ToString().Length <= digits)
+            {
+                if (isPrime(prime))
+                    maxPrime = prime;
+                    //lstNumbers.Items.Add(prime);
+
+                prime++;
+            }
+
+            return prime;
+        }
+
+        bool isPrime(int number)
+        {
+            if (number < 4) return true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
